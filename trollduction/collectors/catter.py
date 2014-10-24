bc36b3bc245e4576fefe0b5d99b4e333d3b272a1
Martin Raspaud <martin.raspaud@smhi.se>
2014-10-24 15:01:56 +0200

    Logging and argparsing in catter.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- trollduction/collectors/catter.py
+++ trollduction/collectors/catter.py
@@ -36,26 +36,59 @@ import os.path
 logger = logging.getLogger(__name__)
 
 config = RawConfigParser()
-config.read("catter.cfg")
 
 
 if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log", help="File to log to (defaults to stdout)",
+                        default=None)
+    parser.add_argument("-v", "--verbose", help="print debug messages too",
+                        action="store_true")
+    parser.add_argument("config", help="config file to be used")
+    opts = parser.parse_args()
+
+    config.read(opts.config)
+
+    if opts.log:
+        import logging.handlers
+        handler = logging.handlers.TimedRotatingFileHandler(opts.log,
+                                                            "midnight",
+                                                            backupCount=7)
+    else:
+        handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[%(levelname)s: %(asctime)s :"
+                                           " %(name)s] %(message)s",
+                                           '%Y-%m-%d %H:%M:%S'))
+    if opts.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    handler.setLevel(loglevel)
+    logging.getLogger('').setLevel(loglevel)
+    logging.getLogger('').addHandler(handler)
+    logging.getLogger("posttroll").setLevel(logging.INFO)
+    logger = logging.getLogger("catter")
+
     subjects = list(set([config.get(section, "subject")
                          for section in config.sections()]))
-    print subjects
+    logger.info("Listening to %s", str(subjects))
     with Publish("catter") as pub:
         with Subscribe("", subjects, addr_listener=True) as sub:
-            print "waiting for messages"
+            logger.info("Ready, waiting for messages")
             for msg in sub.recv():
-                print "got message", msg
+                logger.info("Got message for %s", msg.subject)
                 if msg.type == "collection":
                     mda = msg.data[0].copy()
                     section = (mda["platform"] + " " +
                                mda["number"] + "/" +
                                mda["level"])
-                    print section
                     if section not in config.sections():
+                        logger.debug("Skipping %s", section)
                         continue
+                    logger.debug("Starting catting for %s", section)
                     cat = config.get(section, "cat")
                     pattern = config.get(section, "pattern")
                     mda["proc_time"] = datetime.utcnow()
@@ -70,5 +103,6 @@ if __name__ == '__main__':
                                 out.write(infile.read())
                                 infile.close()
                     new_msg = Message(msg.subject, "file", mda)
-                    print "sending", new_msg
+                    logger.info("Done")
+                    logger.debug("Sending %s", str(new_msg))
                     pub.send(str(new_msg))
