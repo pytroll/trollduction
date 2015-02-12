52641f668dc5e5311053f468a097a2cf5f5a11e9
Martin Raspaud <martin.raspaud@smhi.se>
2015-02-12 17:19:43 +0100

    Replace the corner estimation in region_collector with trollsched's routines.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- trollduction/collectors/region_collector.py
+++ trollduction/collectors/region_collector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2012, 2014 Martin Raspaud
+# Copyright (c) 2012, 2014, 2015 Martin Raspaud
 
 # Author(s):
 
@@ -25,67 +25,15 @@
 """
 
 import os
-import time
 from datetime import timedelta, datetime
 
-import pyproj
-from glob import glob
-
-import numpy as np
-from pyresample import geometry, utils
-from pyorbital.orbital import Orbital
-#from orbitno import get_tle
+from trollsched.satpass import Pass
 
 import logging
 
 LOG = logging.getLogger(__name__)
 
 PLOT = False
-
-tle_names = {"metop a": "metop-a",
-             "metop b": "metop-b",
-             "suomi-npp": "suomi npp",
-             "noaa-19": "noaa19"}
-
-
-def corners(platform, start_time, end_time):
-    """ Compute the corners of a satellite granule
-
-        returns area (BaseDefinition)
-    """
-    #tle = get_tle(platform, start_time)
-
-    orbital = Orbital(platform)
-    track_start = orbital.get_lonlatalt(start_time)
-    track_end = orbital.get_lonlatalt(end_time)
-
-    geod = pyproj.Geod(ellps='WGS84')
-    az_fwd, az_back, dist = geod.inv(track_start[0], track_start[1],
-                                     track_end[0], track_end[1])
-
-    del dist
-
-    NPP_WIDTH = 3000000
-
-    start_left = geod.fwd(track_start[0], track_start[1],
-                          az_fwd - 90, NPP_WIDTH / 2)
-    start_right = geod.fwd(track_start[0], track_start[1],
-                           az_fwd + 90, NPP_WIDTH / 2)
-    end_left = geod.fwd(
-        track_end[0], track_end[1], az_back + 90, NPP_WIDTH / 2)
-    end_right = geod.fwd(
-        track_end[0], track_end[1], az_back - 90, NPP_WIDTH / 2)
-
-    # Check in input data is within the region
-
-    lons = np.array([[start_left[0], end_left[0]],
-                     [start_right[0], end_right[0]]])
-    lats = np.array([[start_left[1], end_left[1]],
-                     [start_right[1], end_right[1]]])
-
-    granule_area = geometry.SwathDefinition(lons, lats)
-    return granule_area
-
 
 class RegionCollector:
 
@@ -121,14 +69,7 @@ class RegionCollector:
         # Check if input data is being waited for
 
         platform = granule_metadata['platform_name']
-        fullname = platform
-        if 'number' in granule_metadata:
-            number = granule_metadata['number']
-            fullname = tle_names.get(fullname + " " + number,
-                                     fullname + " " + number)
-        else:
-            fullname = tle_names.get(fullname.lower(),
-                                     fullname.lower())
+
         start_time = granule_metadata['start_time']
         end_time = granule_metadata['end_time']
 
@@ -155,11 +96,12 @@ class RegionCollector:
             LOG.debug("Estimated granule duration to " +
                       str(self.granule_duration))
 
-        granule_area = corners(fullname, start_time, end_time)
+        granule_pass = Pass(platform, start_time, end_time,
+                            instrument=granule_metadata["sensor"])
 
         # If file is within region, make pass prediction to know what to wait
         # for
-        if granule_area.overlaps(self.region):
+        if granule_pass.area_coverage(self.region) > 0:
             self.granule_times.add(start_time)
             self.granules.append(granule_metadata)
 
@@ -176,18 +118,20 @@ class RegionCollector:
                 gr_time = start_time
                 while True:
                     gr_time += self.granule_duration
-                    gr_area = corners(fullname, gr_time,
-                                      gr_time + self.granule_duration)
-                    if not gr_area.overlaps(self.region):
+                    gr_pass = Pass(platform, gr_time,
+                                   gr_time + self.granule_duration,
+                                   instrument=granule_metadata["sensor"])
+                    if not gr_pass.area_coverage(self.region) > 0:
                         break
                     self.planned_granule_times.add(gr_time)
 
                 gr_time = start_time
                 while True:
                     gr_time -= self.granule_duration
-                    gr_area = corners(fullname, gr_time,
-                                      gr_time + self.granule_duration)
-                    if not gr_area.overlaps(self.region):
+                    gr_pass = Pass(platform, gr_time,
+                                   gr_time + self.granule_duration,
+                                   instrument=granule_metadata["sensor"])
+                    if not gr_pass.area_coverage(self.region) > 0:
                         break
                     self.planned_granule_times.add(gr_time)
 
@@ -247,52 +191,3 @@ def read_granule_metadata(filename):
             metadata[attr] = datetime.strptime(
                 metadata[attr], "%Y-%m-%dT%H:%M:%S")
     return metadata
-
-
-if __name__ == '__main__':
-
-    LOG.info("Welcome to pytroll")
-
-    input_dir = "tests/data"
-
-    region = utils.load_area(
-        '/home/a001673/usr/src/satprod/etc/areas.def', 'scan1')
-    LOG.debug("Read area " + str(region))
-
-    NPP_GRANULE_DURATION = timedelta(seconds=85.75)
-
-    time_diff = datetime.utcnow() - datetime(2014, 10, 17, 11, 34, 39)
-
-    collector = RegionCollector(region,
-                                timeliness=time_diff + timedelta(seconds=5),
-                                granule_duration=NPP_GRANULE_DURATION)
-    old_granules = set()
-
-    try:
-        while True:
-
-            input_granules = set(glob(os.path.join(input_dir, "npp*.json")))
-
-            input_granules -= old_granules
-
-            swath = None
-
-            for input_granule in sorted(list(input_granules)):
-                metadata = read_granule_metadata(input_granule)
-                swath = swath or collector.collect(metadata)
-                old_granules.add(input_granule)
-
-            if swath is not None:
-                break
-
-            if (collector.timeout is not None) and datetime.utcnow() > collector.timeout:
-                LOG.warning("Timeout! (" + collector.timeout.isoformat() + ")")
-                swath = collector.granules
-                break
-
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        pass
-
-    print swath
