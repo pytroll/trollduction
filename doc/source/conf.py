a8c30040e255544e35d72a38a415c92290a0f04c
Martin Raspaud <martin.raspaud@smhi.se>
2015-02-19 14:07:54 +0100

    mock mpop for building documentation.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,6 +16,32 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        elif name == "inf":
+            return 0
+        else:
+            return Mock()
+
+MOCK_MODULES = ['mpop',]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
