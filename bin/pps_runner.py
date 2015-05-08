8607f4be97b500e0dab33de0f34027e7d82195b8
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2015-05-08 11:59:26 +0200

    Don't listen to the SDR_compact (EARS-VIIRS) data. PPS is not compatible with this format
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- bin/pps_runner.py
+++ bin/pps_runner.py
@@ -528,7 +528,7 @@ def pps_rolling_runner():
 
     files4pps = {}
     threads = []
-    with posttroll.subscriber.Subscribe("", ['AAPP-HRPT', 'EOS/1', 'SDR'],
+    with posttroll.subscriber.Subscribe("", ['AAPP-HRPT', 'EOS/1', 'SDR/1B'],
                                         True) as subscr:
         with Publish('pps_runner', 0, ['PPS', ]) as publisher:
             while True:
