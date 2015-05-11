8b73b005ea5153b041d8a988b57f4d77881f494c
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2015-05-11 21:45:28 +0200

    Allow for different paths for hdf5/netcdf and xml output
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- bin/pps_runner.py
+++ bin/pps_runner.py
@@ -145,17 +145,17 @@ def get_outputfiles(path, satid, orb):
     h5_output = (os.path.join(path, 'S_NWC') + '*' +
                  str(METOP_NAME_LETTER.get(satid, satid)) +
                  '_' + str(orb) + '*.h5')
-    LOG.debug(
+    LOG.info(
         "Match string to do a file globbing on hdf5 output files: " + str(h5_output))
     nc_output = (os.path.join(path, 'S_NWC') + '*' +
                  str(METOP_NAME_LETTER.get(satid, satid)) +
                  '_' + str(orb) + '*.nc')
-    LOG.debug(
+    LOG.info(
         "Match string to do a file globbing on netcdf output files: " + str(nc_output))
     xml_output = (os.path.join(path, 'S_NWC') + '*' +
                   str(METOP_NAME_LETTER.get(satid, satid)) +
                   '_' + str(orb) + '*statistics.xml')
-    LOG.debug(
+    LOG.info(
         "Match string to do a file globbing on xml output files: " + str(xml_output))
     return glob(h5_output) + glob(nc_output) + glob(xml_output)
 
@@ -239,10 +239,15 @@ def pps_worker(publisher, scene, semaphore_obj, queue):
         "Ready with PPS level-2 processing on scene: " + str(scene))
 
     # Now check what netCDF/hdf5 output was produced and publish them:
+    pps_path = my_env.get('SM_PRODUCT_DIR', PPS_OUTPUT_DIR)
     result_files = get_outputfiles(
-        PPS_OUTPUT_DIR, SATELLITE_NAME[scene['satid']], scene['orbit_number'])
-    LOG.info("Output files: " + str(result_files))
-    queue.put((publisher, scene, result_files))
+        pps_path, SATELLITE_NAME[scene['satid']], scene['orbit_number'])
+    LOG.info("PPS Output files: " + str(result_files))
+    pps_path = my_env.get('STATISTICS_DIR', PPS_OUTPUT_DIR)
+    xml_files = get_outputfiles(
+        pps_path, SATELLITE_NAME[scene['satid']], scene['orbit_number'])
+    LOG.info("PPS summary statistics files: " + str(xml_files))
+    queue.put((publisher, scene, result_files + xml_files))
     semaphore_obj.release()
     return
 
