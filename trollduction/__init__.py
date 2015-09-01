8a0de30f94779105affdf1ec4dacc9fd6caba5e7
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2015-09-01 19:19:35 +0200

    Dynamic checking of hostname
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

---
+++ trollduction/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Adam.Dybbroe
+
+# Author(s):
+
+#   Adam.Dybbroe <a000680@c20671.ad.smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""trollduction package - helper functions
+"""
+
+import netifaces
+
+
+def get_local_ips():
+    inet_addrs = [netifaces.ifaddresses(iface).get(netifaces.AF_INET)
+                  for iface in netifaces.interfaces()]
+    ips = []
+    for addr in inet_addrs:
+        if addr is not None:
+            for add in addr:
+                ips.append(add['addr'])
+    return ips
