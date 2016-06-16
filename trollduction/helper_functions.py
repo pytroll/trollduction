32ff4c734c8024e9326b409d5798442d41bbe98f
Christian Kliche <chk@ebp.de>
2016-06-16 16:19:31 +0200

    Support for parametrized composites
    The product configuration can be modified to allow
    parametrized composites:
    <product id="sample_comp" name="my_sample">
      <composite_params>
        <param1>[0.0, 0.3, 0.0]</param1>
        <paramX>None</paramX>
        ..
      </composite_params>
      <file>sample.tif</file>
    </product>

--- trollduction/helper_functions.py
+++ trollduction/helper_functions.py
@@ -433,3 +433,16 @@ def parse_aliases(config):
                 alias = aliases2
             aliases[new_key] = alias
     return aliases
+
+
+def eval_default(expression, default_res=None):
+    """Calls eval on expression and returns default_res if it throws
+    exceptions
+    """
+    if default_res is None:
+        default_res = expression
+    try:
+        return eval(expression)
+    except:
+        pass
+    return default_res
