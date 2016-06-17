f6763c04d273363bb7f922cd66945d33a5b7de23
Panu Lahtinen <pnuu+git@iki.fi>
2016-06-17 15:52:27 +0300

    Move queue initial value to correct place

--- trollflow/generate_process.py
+++ trollflow/generate_process.py
@@ -5,7 +5,6 @@ def generate_daemon(config_item):
     return config_item['components'][-1]['class']
 
 def generate_workflow(config_item):
-    print config_item
     wfs = WorkflowStreamer(config=config_item)
     wfs.start()
     return wfs
@@ -25,9 +24,8 @@ workers = []
 for item in config['work']:
     workers.append(types[item['type']](item))
 
+queue = None
 for worker in workers:
-    queue = None
-
     if queue is not None:
         worker.input_queue = queue
     queue = worker.output_queue
