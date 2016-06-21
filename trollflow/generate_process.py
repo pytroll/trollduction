d107f915f9bdf2b72e0861a0fafdee958beb6f78
Panu Lahtinen <pnuu+git@iki.fi>
2016-06-21 13:53:27 +0300

    Add logging and log config

--- trollflow/generate_process.py
+++ trollflow/generate_process.py
@@ -1,3 +1,7 @@
+
+import logging
+import logging.config
+
 from trollflow.workflow_launcher import WorkflowStreamer
 
 
@@ -16,6 +20,13 @@ path_to_workflow = 'test/flow.yaml'
 with open(path_to_workflow, "r") as fid:
     config = yaml.load(fid)
 
+for item in config["config"]:
+    if "log_config" in item.keys():
+        logging.config.fileConfig(item["log_config"])
+        break
+logger = logging.getLogger("main")
+logger.info("Initializing pluginized Trollduction")
+
 types = {'daemon': generate_daemon,
          'workflow': generate_workflow}
 
@@ -29,3 +40,5 @@ for worker in workers:
     if queue is not None:
         worker.input_queue = queue
     queue = worker.output_queue
+
+logger.info("Ready to process new scenes")
