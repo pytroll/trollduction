7d8b8e0c9e31ecd9fae8eea49012f42ee285eb1e
Panu Lahtinen <pnuu+git@iki.fi>
2016-06-21 10:55:24 +0300

    Add more config file options, add logging

--- trollduction/plugins/compositor.py
+++ trollduction/plugins/compositor.py
@@ -1,6 +1,13 @@
 """Classes for generating image composites for Trollflow based Trollduction"""
 
+import os.path
+import logging
+
 from trollflow.workflow_component import AbstractWorkflowComponent
+from trollduction.xml_read import ProductList
+from trollsift import compose
+
+LOGGER = logging.getLogger("DataWriter")
 
 class CompositeGenerator(AbstractWorkflowComponent):
 
@@ -17,16 +24,45 @@ class CompositeGenerator(AbstractWorkflowComponent):
     def invoke(context):
         """Invoke"""
         data = context["content"]
+        prod_list = ProductList(context["product_list"]["content"])
         for prod in data.info["products"]:
-            print "Creating composite", prod
+            LOGGER.info("Creating composite %s", prod)
             func = getattr(data.image, prod)
             img = func()
-            # TODO: Get filename pattern from config?
-            file_items = None
-            params = None
-            context["output_queue"].put((img, file_items, params))
-            img.show()
+            # Get filename from product config
+            fname = create_fname(data.info, prod_list, prod)
+            if fname is None:
+                LOGGER.error("Could not generate a valid filename, "
+                              "product not saved!")
+            else:
+                context["output_queue"].put((img, fname))
+            # img.show()
 
     def post_invoke(self):
         """Post-invoke"""
         pass
+
+def create_fname(info, prod_list, prod):
+    """Create filename for product *prod*"""
+    area = info["area"]
+    info["productname"] = prod
+    info["areaname"] = area
+    pattern = _get_pattern_xml(prod_list, area, prod)
+    if pattern is not None:
+        return compose(pattern, info)
+    else:
+        return None
+
+def _get_pattern_xml(prod_list, area_name, prod_name):
+    """Get filepattern for area *area* and product *prod*"""
+    for grp in prod_list.groups:
+        for area in grp.data:
+            if area.attrib["name"] != area_name:
+                continue
+            for product in area:
+                if product.attrib["name"] == prod_name:
+                    output_dir = \
+                        product.attrib.get("output_dir",
+                                           prod_list.attrib["output_dir"])
+                    return os.path.join(output_dir, product[0].text)
+    return None
